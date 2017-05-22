c8ef03fc02fe034bb0ca8edaa03527c8e452c1ad
Patrick José Pereira <patrickelectric@gmail.com>
2017-05-22 20:04:42 -0300

    Add opencvpos
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- robot.py
+++ robot.py
@@ -1,9 +1,10 @@
 import cv2
 from interface import RobotInterface
 from input.keylistener import KeyListener
-
+from opencvpos import OpencvPos
 
 interface = RobotInterface()
+opencvpos = OpencvPos()
 
 interface.gripper.move((0, 0, 0.02), False)
 keys = KeyListener()
@@ -11,6 +12,19 @@ keys = KeyListener()
 
 while True:
     img = interface.get_image_from_camera()
+
+    try:
+        perc, tag_name, tag_image, tag_detec, points = opencvpos.get_position_from_image(img)
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+        cv2.polylines(img,[points], True, (0,255,255))
+        tag_image = cv2.cvtColor(tag_image, cv2.COLOR_GRAY2RGB)
+        tag_detec = cv2.cvtColor(tag_detec, cv2.COLOR_GRAY2RGB)
+        img[0:tag_image.shape[1], img.shape[0]-tag_image.shape[0]:img.shape[0]] = tag_image
+        img[tag_image.shape[1]:tag_image.shape[1] + tag_detec.shape[1], img.shape[0]-tag_image.shape[0]:img.shape[0]] = tag_detec
+    except:
+        print('Error in opencvpos!')
+        pass
+
     cv2.imshow("target", img)
     ch = cv2.waitKey(5) & 0xFF
 
