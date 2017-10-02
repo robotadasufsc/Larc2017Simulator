63bfae44e8410425d8ea1b5621aae4b54df6e1b2
Willian Galvani <williangalvani@gmail.com>
2017-10-02 14:25:34 -0300

    add manual mode, state machine

--- navigator.py
+++ navigator.py
@@ -13,27 +13,31 @@ class Navigator():
         if self.state == "WHERETHEFUCKAMI":
 
             # TODO: replace with a decent controller
-            if tag_name == "y.png":
+            if not (tag_name is None):
+                if tag_name == "Y.png":
 
-                # TODO: we need to avoid abruptal direction changes on the wheels, as the escs have
-                # a required timeout
+                    # TODO: we need to avoid abruptal direction changes on the wheels, as the escs have
+                    # a required timeout
 
-                if tag_angle is not None and tag_angle[0] > 4:
-                    self.interface.set_right_speed(-0.5)
-                    self.interface.set_left_speed(0.5)
-                elif tag_angle is not None and tag_angle[0] < -4:
-                    self.interface.set_right_speed(0.5)
-                    self.interface.set_left_speed(-0.5)
+                    if tag_angle is not None and tag_angle[0] > 4:
+                        self.interface.set_right_speed(-0.5)
+                        self.interface.set_left_speed(0.5)
+                    elif tag_angle is not None and tag_angle[0] < -4:
+                        self.interface.set_right_speed(0.5)
+                        self.interface.set_left_speed(-0.5)
+                    else:
+                        self.interface.set_right_speed(0.5)
+                        self.interface.set_left_speed(0.5)
+
+                    if sensors['fr'][0]:
+                        #got to a wall?
+                        self.state = "GETHEFUCKINGCUP"
+
                 else:
-                    self.interface.set_right_speed(0.5)
-                    self.interface.set_left_speed(0.5)
-
-                if sensors['fr'][0]:
-                    #got to a wall?
-                    self.state = "GETHEFUCKINGCUP"
-
-            else:
-                self.state = "GENERALIZEDCHAOS"
+                    print(tag_name, tag_name is None)
+                    print("FUCK, GOT ", tag_name, tag_name is not None, type(tag_name))
+                    self.state = "GENERALIZEDCHAOS"
 
 
         if self.state == "GETTHEFUCKINGCUP":
+            print("should be trying to get the cup, now")
